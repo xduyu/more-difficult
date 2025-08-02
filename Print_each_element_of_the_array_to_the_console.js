cf538093a3f4a18ff1450f9e9f14ae91b1e13ff4
rodya <rdstmn11@gmail.com>
2025-08-03 00:24:05 +0500

    peeotattc - f - test

---
+++ Print_each_element_of_the_array_to_the_console.js
@@ -0,0 +1,10 @@
+function peeotattc(arr) {
+    if (!Array.isArray(arr)) {
+        return "Input is not a valid array"; // Check if input is an array
+    }
+    
+    arr.forEach(element => {
+        console.log(element); // Print each element to the console
+    });
+    
+}
