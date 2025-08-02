959e763eeb6a8afb591512d17df066bcd52458ac
rodya <rdstmn11@gmail.com>
2025-08-03 00:04:33 +0500

    itnigt - f - test

---
+++ If_the_number_is_greater_than.js
@@ -0,0 +1,10 @@
+function itn_i_g_h(n) {
+    if (num > 10) {
+        return "The number is greater than 10";
+    } else {
+        return "The number is not greater than 10";
+    }
+}
+
+var num = 15; // Example number
+console.log(itn_i_g_h(num)); // Output: The number is greater than 10
