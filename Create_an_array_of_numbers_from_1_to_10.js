1b14b173aabe9f572ce506c76c516af8c143210b
rodya <rdstmn11@gmail.com>
2025-08-03 00:26:32 +0500

    caaonf1t10 - f - test

---
+++ Create_an_array_of_numbers_from_1_to_10.js
@@ -0,0 +1,9 @@
+function caaonf1t10() {
+    let arr = [];
+    for (let i = 1; i <= 10; i++) {
+        arr.push(i); // Add numbers from 1 to 10 to the array
+    }
+    return arr; // Return the array
+}
+
+console.log(caaonf1t10()); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
