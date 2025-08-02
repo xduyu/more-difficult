a2836669424f4296637dfb8327b8a5f3d84123a9
rodya <rdstmn11@gmail.com>
2025-08-03 00:16:18 +0500

    ctnoeia_arr - f - test

---
+++ Count_the_number_of_elements_in_an_array.js
@@ -0,0 +1,8 @@
+function ctnoeia_arr(arr) {
+    if (!Array.isArray(arr)) {
+        return "Input is not a valid array"; // Check if input is an array
+    }
+    
+    return arr.length; // Return the length of the array
+    
+}
